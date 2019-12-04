57962e6b7fdbbde9029a563867a5ad7b84be4b6b
Иван Козловский <ikozlovskiy24@gmail.com>
2019-12-04 13:13:41 +0300

    Аргументы могут быть расположены в любом порядке Рефакторинг

--- README.md
+++ README.md
@@ -4,34 +4,44 @@
 
 ## Начало
 
-> **[python 3.4](https://python.org/) или новее**
+> [python 3.4](https://python.org/) или новее**
 
     $ git clone https://github.com/ikozlovsky/vkAudioMessageUploader.git
     $ cd vkAudioMessageUploader
     $ pip3 install -r requirements.txt
 
-> Либо скачайте отдельно файл audiomsg.py, разместите его в удобном месте и установите зависимость через pip: 
+> Либо скачайте отдельно файл audiomsg.py, разместите его в удобном месте и установите библиотеку [vk_api](https://github.com/python273/vk_api) через pip: 
 
     $ pip3 install vk_api
 
 ## Запуск
 
-> Подставьте своё значение переменной __ACCESS_TOKEN__ в __audiomsg.py__ и запустите скрипт:
-
-    $ python3 audiomsg.py [ДИАЛОГ] [ПУТЬ ДО ФАЙЛА 1] ... [ПУТЬ ДО ФАЙЛА n]
-
-> Так же токен можно передать первым аргументом, тогда скрипт не обязательно редактировать:
+> Запустите скрипт (все аргументы могут следовать в произвольном порядке):
 
     $ python3 audiomsg.py [ACCESS_TOKEN] [ДИАЛОГ] [ПУТЬ ДО ФАЙЛА 1] ... [ПУТЬ ДО ФАЙЛА n]
 
-> Аргумент [ДИАЛОГ] представляет собой либо числовой id диалога (id беседы можно указать в кратком формате, начав его с символа "@": @97, либо в полном формате: 2000000097), либо строку, входящую в название диалога (поиск осуществляется по 50 последним диалогам), пробелы в этом аргументе следует экранировать.
+> Либо подставьте своё значение переменной __ACCESS_TOKEN__ в __audiomsg.py__, чтобы не указывать его в аргументах каждый раз и запустите скрипт  (все аргументы могут следовать в произвольном порядке):
+
+    $ python3 audiomsg.py [ДИАЛОГ] [ПУТЬ ДО ФАЙЛА 1] ... [ПУТЬ ДО ФАЙЛА n]    % знерщт3 фгвшщьыпюзн ~LBFKJU` ~GENM LJ AFQKF 1` &&& ~GENM LJ AFQKF тъ
+
+
+> Аргумент [ДИАЛОГ] представляет собой либо числовой id диалога (id беседы можно указать в кратком формате, начав его с символа "@": @97, либо в полном формате: 2000000097), либо строку для поиска среди 50 последних диалогов.
 
 > Примеры:
 
-    $ python3 audiomsg.py d1e45caf232775ada422fed97544cd2bc5bca69a918ac0f7b22d625d209a6bb9b6c2760767865111c3ed4 антон\ королёв /Users/ivan/Downloads/bbae553bfe.mp3
-    $ python3 audiomsg.py сберкот /Users/ivan/Downloads/bbae553bfe.mp3 /Users/ivan/Downloads/f0ec2572a2.mp3
+    $ python3 антон королёв audiomsg.py d1e45caf232775ada422fed97544cd2bc5bca69a918ac0f7b22d625d209a6bb9b6c2760767865111c3ed4  /Users/ivan/Downloads/bbae553bfe.mp3 
+    Отправка в: 94388048
+    $ python3 audiomsg.py d1e45caf232775ada422fed97544cd2bc5bca69a918ac0f7b22d625d209a6bb9b6c2760767865111c3ed4 антон королёв /Users/ivan/Downloads/bbae553bfe.mp3
+    Отправка в: 94388048
+    $ python3 audiomsg.py /Users/ivan/Downloads/bbae553bfe.mp3 /Users/ivan/Downloads/f0ec2572a2.mp3 сберкот
+    Отправка в: -157369801
     $ python3 audiomsg.py @97 /Users/ivan/Downloads/bbae553bfe.mp3
+    Отправка в: 2000000097
     $ python3 audiomsg.py 2000000097 /Users/ivan/Downloads/bbae553bfe.mp3
+    Отправка в: 2000000097
     $ python3 audiomsg.py 94388048 /Users/ivan/Downloads/bbae553bfe.mp3
+    Отправка в: 94388048
 
 Получить токен можно тут: <http://oauth.vk.com/authorize?client_id=2685278&display=mobile&redirect_uri=https://oauth.vk.com/blank.html&scope=725086&response_type=token&v=5.101&revoke=1>
+
+Чтобы не вводить каждый раз __python3 audiomsg.py__, можно создать alias под удобным именем в скрипте инициализации вашего интерпретатора (для __audiomsg.py__, лежащего в корне домашней папки, и интерпретатора bash, в конец __~/.bashrc__ добавить __alias audiomsg="python3 ~/audiomsg.py"__).
--- audiomsg.py
+++ audiomsg.py
@@ -2,43 +2,87 @@ import vk_api
 import sys
 import random
 import time
+import os
 
 ACCESS_TOKEN = ""
 
-file = []
-audio_ids = []
-chat_id=0
-
-if len(sys.argv) == 1:
-    print("usage: python3 audiomsg.py [ACCESS_TOKEN] [DIALOG SEARCH STRING] [[@]CHAT_ID] [FILE1] [FILE2] ... [FILEN]")
-    exit()
-
-try:
-    if sys.argv[1][0] == "@":
-        int(sys.argv[1][1:])
-    else:
-        int(sys.argv[1])
-    sys.argv=sys.argv[1:]
-except ValueError:
-    if len(sys.argv[1]) > 50:
-        try:
-            int(sys.argv[1],base=16)
-            ACCESS_TOKEN = sys.argv[1]
-            sys.argv=sys.argv[2:]
-        except ValueError:
-            pass
-    else:
-        sys.argv=sys.argv[1:]
 vk_session = vk_api.VkApi(token=ACCESS_TOKEN)
 vk = vk_session.get_api()
 upload = vk_api.upload.VkUpload(vk)
-try:
-    if sys.argv[0][0] == "@":
-        int(sys.argv[0][1:])
-    else:
-        int(sys.argv[0])
-    chat_id = sys.argv[0]
-except ValueError:
+
+def sendAudio(peer_id, files):
+    print("Отправка в:", peer_id)
+    audio_ids = []
+    for i in files:
+        re = upload.audio_message(i)['audio_message']
+        audio_ids.append("doc" + str(re['owner_id']) + "_" + str(re['id']))
+    for i in audio_ids:
+        while True:
+            try:
+                vk.messages.send(peer_id=peer_id, attachment=i,random_id=random.getrandbits(64))
+            except vk_api.exceptions.Captcha:
+                time.sleep(2)
+                continue
+            break
+        time.sleep(0.7)
+
+def parseArgs(args):
+    searchSequence = ""
+    files = []
+    peer_id = 0
+    if args[0].find(".py"):
+        args=args[1:]
+    for i in args:
+        if isToken(i):
+            authorize(i)
+        elif isID(i):
+            peer_id = i
+        elif isFile(i):
+            files.append(i)
+        else:
+            if i[0] == "@":
+                peer_id = 2000000000 + int(i[1:])
+            else:
+                searchSequence += i + " "
+    searchSequence = searchSequence[:-1]
+    if searchSequence and not peer_id:
+        peer_id = searchForId(searchSequence)
+        if peer_id == True:
+            print("Больше одного совпадения.")
+            exit()
+        elif peer_id == False:
+            print("Диалог не найден.")
+            exit()
+    elif not searchSequence and not peer_id:
+        peer_id = vk.users.get()[0]['id']
+    return(peer_id, files)
+
+def authorize(ACCESS_TOKEN):
+    global vk, upload
+    vk_session = vk_api.VkApi(token=ACCESS_TOKEN)
+    vk = vk_session.get_api()
+    upload = vk_api.upload.VkUpload(vk)
+
+def isToken(s):
+    try:
+        int(s,16)
+        return True
+    except ValueError:
+        return False
+
+
+def isFile(s):
+    return os.path.isfile(s)
+
+def isID(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+def searchForId(s):
+    s = s.lower()
     c = vk.messages.getConversations(count=50,extended=1,fields="first_name,last_name")
     p = []
     g = []
@@ -65,44 +109,19 @@ except ValueError:
             conversations[-i['id']] = i['name']
     conversations = {j.lower(): i for i, j in conversations.items()}
     count = 0
-    chat_id = 0
+    peer_id = 0
     for i, j in conversations.items():
-        if i.find(sys.argv[0].lower()) != -1:
-            chat_id = j
-            count+=1
+        if i.find(s) != -1:
+            peer_id = j
+            count += 1
     if count == 0:
-        print("Диалог не найден.")
-        exit()
+        return False
     elif count == 1:
-        print("Отправка в", chat_id)
-        chat_id = str(chat_id)
+        return peer_id
     else:
-        print("Больше одного совпадения.")
-        exit()
+        return True
 
-for i in range(1,len(sys.argv)):
-    file.append(sys.argv[i])
-
-for i in file:
-    re = upload.audio_message(i)['audio_message']
-    audio_ids.append("doc" + str(re['owner_id']) + "_" + str(re['id']))
-if chat_id[0] == "@":
-    for i in audio_ids:
-        while True:
-            try:
-                vk.messages.send(chat_id=chat_id[1:], attachment=i,random_id=random.getrandbits(64))
-            except vk_api.exceptions.Captcha:
-                time.sleep(2)
-                continue
-            break
-        time.sleep(0.7)
-else:
-    for i in audio_ids:
-        while True:
-            try:
-                vk.messages.send(peer_id=chat_id, attachment=i,random_id=random.getrandbits(64))
-            except vk_api.exceptions.Captcha:
-                time.sleep(2)
-                continue
-            break
-        time.sleep(0.7)
+if __name__ == "__main__":
+    if ACCESS_TOKEN != "":
+        authorize(ACCESS_TOKEN)
+    sendAudio(*parseArgs(sys.argv))
