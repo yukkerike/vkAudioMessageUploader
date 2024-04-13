654ca49e0a236e7539a87a61c19fcc5aad6aa477
Ivan Kozlovsky <ikozlovskiy24@gmail.com>
2024-04-13 10:28:41 +0300

    АХАХАХАХАХАХАХАХАХАХАХАХАХАХАХАХА
    кринге...

--- audiomsg.py
+++ audiomsg.py
@@ -1,141 +1,135 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+import os
+import random
+import sys
+import time
 
 import vk_api
-import sys
-import random
-import time
-import os
+from vk_api.upload import VkUpload
 
-ACCESS_TOKEN = ""
+ACCESS_TOKEN = ""  # Вставьте свой токен
+vk_session = vk = upload = None
 
-vk = upload = None
-
-def sendAudio(peer_id, files):
-    print("Отправка в:", peer_id)
+def send_audio(peer_id: int, files: list[str]) -> None:
+    print(f"Получатель: {peer_id}")
     audio_ids = []
     files_count = len(files)
     success_count = 0
-    print(f'Загрузка [0/{files_count}]', end='')
-    symbols_to_flush = len(str(files_count))+3
-    for i in files:
-        success_count+=1
-        print('\b'*symbols_to_flush+f'{success_count}/{files_count}]', end='', flush=True)
-        re = upload.audio_message(i)['audio_message']
-        audio_ids.append("doc" + str(re['owner_id']) + "_" + str(re['id']))
-        symbols_to_flush = len(str(success_count)+str(files_count))+2
+
+    print(f"Выгрузка [0/{files_count}]", end="")
+    symbols_to_flush = len(str(files_count)) + 3
+    faulty_files = []
+    for i, file_path in enumerate(files):
+        success_count += 1
+        print("\b" * symbols_to_flush + f"{success_count}/{files_count}]", end="", flush=True)
+        try:
+            response = upload.audio_message(file_path)["audio_message"]
+            audio_ids.append(f"doc{response['owner_id']}_{response['id']}")
+            symbols_to_flush = len(str(success_count) + str(files_count)) + 2
+        except Exception:
+            faulty_files.append(file_path)
     print()
+    if len(faulty_files):
+        print("Возникли проблемы с отправкой файлов:", faulty_files)
+
     audios_count = len(audio_ids)
     success_count = 0
-    print(f'Отправка [0/{audios_count}]', end='')
-    symbols_to_flush = len(str(audios_count))+3   
-    for i in audio_ids:
-        success_count+=1
-        print('\b'*symbols_to_flush+f'{success_count}/{files_count}]', end='', flush=True)
-        while True:
-            try:
-                vk.messages.send(peer_id=peer_id, attachment=i,random_id=random.getrandbits(64))
-            except vk_api.exceptions.Captcha:
-                time.sleep(2)
-                continue
-            break
-        symbols_to_flush = len(str(success_count)+str(files_count))+2
+    print(f"Отправка [0/{audios_count}]", end="")
+    symbols_to_flush = len(str(audios_count)) + 3
+    for i, audio_id in enumerate(audio_ids):
+        success_count += 1
+        print("\b" * symbols_to_flush + f"{success_count}/{files_count}]", end="", flush=True)
+        debounce(lambda p, a: vk.messages.send(peer_id=p, attachment=a, random_id=random.getrandbits(64)), peer_id, i)
+        symbols_to_flush = len(str(success_count) + str(files_count)) + 2
         time.sleep(0.7)
+
     print()
 
-def parseArgs(args):
-    searchSequence = ""
+def debounce(f, *args) -> None:
+    while True:
+        try:
+            f(*args)
+        except vk_api.exceptions.Captcha:
+            time.sleep(2)
+            continue
+        break
+
+def parse_args(args: list[str]) -> tuple[int, list[str]]:
+    search_sequence = ""
     files = []
     peer_id = 0
-    if args[0].find(".py"):
-        args=args[1:]
-    for i in args:
-        if getID(i):
-            peer_id = getID(i)
-        elif isToken(i):
-            authorize(i)
-        elif isFile(i):
-            files.append(i)
+
+    for arg in args:
+        if get_id(arg):
+            peer_id = get_id(arg)
+        elif is_token(arg):
+            authorize(arg)  # Update vk and upload objects
+        elif is_file(arg):
+            files.append(arg)
         else:
-            searchSequence += i + " "
-    searchSequence = searchSequence[:-1]
-    if searchSequence and not peer_id:
-        peer_id = searchForId(searchSequence)
-        if peer_id == True:
-            print("Больше одного совпадения.")
-            exit()
-        elif peer_id == False:
-            print("Диалог не найден.")
-            exit()
-    elif not searchSequence and not peer_id:
-        peer_id = vk.users.get()[0]['id']
-    return(peer_id, files)
+            search_sequence += arg + " "
 
-def authorize(ACCESS_TOKEN):
-    global vk, upload
-    vk_session = vk_api.VkApi(token=ACCESS_TOKEN)
+    search_sequence = search_sequence.strip()
+
+    if search_sequence and not peer_id:
+        peer_id = search_for_id(search_sequence)
+        if type(peer_id) is bool:
+            print("More than one match found." if peer_id else "Dialogue not found.")
+            sys.exit(1)
+    elif not peer_id:
+        peer_id = vk.users.get()[0]["id"]
+
+    return peer_id, files
+
+def authorize(access_token: str) -> None:
+    global vk_session, vk, upload
+    vk_session = vk_api.VkApi(token=access_token)
     vk = vk_session.get_api()
-    upload = vk_api.upload.VkUpload(vk)
+    upload = VkUpload(vk)
 
-def isToken(s):
-    try:
-        int(s,16)
-        return True
-    except ValueError:
-        if s.find("vk") == 0:
-            return True
-    return False
+def is_token(string: str) -> bool:
+    return string.startswith("vk") # пришло время отказаться от токенов старого образца
 
-def isFile(s):
-    return os.path.isfile(s)
+def is_file(path: str) -> bool:
+    return os.path.isfile(path)
 
-def getID(s):
-    try:
-        if s[0] == "@":
-            return int(s[1:]) + 2000000000
-        return int(s)
-    except ValueError:
-        return
+def get_id(string: str) -> int:
+    if string[0] == "@":
+        return int(string[1:]) + 2000000000
+    return string.isnumeric() and int(string)
 
-def searchForId(s):
-    s = s.lower()
-    c = vk.messages.getConversations(count=50,extended=1,fields="first_name,last_name")
-    p = []
-    g = []
-    try:
-        p = c['profiles']
-    except KeyError:
-        pass
-    try:
-        g = c['groups']
-    except KeyError:
-        pass
-    c = c['items']
+def search_for_id(query: str) -> int | bool:
+    query = query.lower()
+    conversations_response = vk.messages.getConversations(count=50, extended=1, fields="first_name,last_name")
+    profiles = conversations_response.get("profiles", [])
+    groups = conversations_response.get("groups", [])
     conversations = {}
-    for i in c:
-        if i['conversation']['peer']['type'] == "user" or i['conversation']['peer']['type'] == "group":
-            conversations[i['conversation']['peer']['id']]=""
-        elif i['conversation']['peer']['type'] == "chat":
-            conversations[i['conversation']['peer']['id']]=i['conversation']['chat_settings']['title'].lower()
-    for i in p:
-        if i['id'] in conversations:
-            conversations[i['id']] = (i['first_name'] + " " + i['last_name']).lower()
-    for i in g:
-        if -i['id'] in conversations:
-            conversations[-i['id']] = i['name'].lower()
-    count = 0
+    for item in conversations_response["items"]:
+        peer_type = item["conversation"]["peer"]["type"]
+        peer_id = item["conversation"]["peer"]["id"]
+        conversations[peer_id] = item["conversation"]["chat_settings"]["title"].lower() if peer_type == "chat" else ""
+
+    for profile in profiles:
+        if profile["id"] in conversations:
+            conversations[profile["id"]] = (profile["first_name"] + " " + profile["last_name"]).lower()
+
+    for group in groups:
+        if -group["id"] in conversations:
+            conversations[-group["id"]] = group["name"].lower()
+
+    matched = False
     peer_id = 0
-    for i, j in conversations.items():
-        if j.find(s) != -1:
-            peer_id = i
-            count += 1
-    if count == 0:
-        return False
-    elif count == 1:
-        return peer_id
-    else:
-        return True
+    for id_, name in conversations.items():
+        if matched:
+            return matched
+        matched = query in name
+        if matched:
+            peer_id = id_
+    return matched and peer_id
+
+if ACCESS_TOKEN:
+    authorize(ACCESS_TOKEN)
 
 if __name__ == "__main__":
-    if ACCESS_TOKEN != "":
-        authorize(ACCESS_TOKEN)
-    sendAudio(*parseArgs(sys.argv))
+    send_audio(*parse_args(sys.argv[1:]))
